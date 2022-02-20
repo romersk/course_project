cc5bb0e7fa95285b4827f195997b5595d8096197
Roman <evdockim0vr@yandex.by>
2022-02-20 23:58:12 +0300

    Initialization of the project

---
+++ src/main/java/com/bsuir/CpEvdokimovApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpEvdokimovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpEvdokimovApplication.class, args);
+	}
+
+}
